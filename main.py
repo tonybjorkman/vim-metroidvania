7367ed207dfec00d28687bbce8c70e48b9c092e8
Tony Björkman <tony_bjorkman@outlook.com>
2022-02-06 14:47:59 +0100

    added spritesheet and map class which now prints the fontsheet, character by character onto screen

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pygame 
 import random
 import spritesheet
-
+import math
 
 class Player:
-    def __init__(self, sprites):
+
+    def __init__(self, fonts):
         self.x = 0
         self.y = 0
-        self.sprites = sprites
+        self.sprites = fonts
         self.sprite_x = 0
         self.sprite_y = 0
         self.sprite = None
         self.update_sprite()
-    
+     
     def move(self, x, y):
         self.x += x
         self.y += y
@@ -36,9 +37,47 @@ class Player:
         if self.sprite_y > len(self.sprites)-1:
             self.sprite_y = 0
         self.update_sprite()
+    
+
+class FontSpriteSheet:
+
+    def __init__(self) -> None:
+        self.sheet = spritesheet.spritesheet('fonts.png')
+        self.images = self.sheet.load_matrix([0, 0, 20, 20],(15,8), -1)
+
+    def get_char(self, char):
+        code = ord(char)
+        if code < 32 or code > 126:
+            code = 32
+        return code
+    
+    # convert between ascii and font sprite
+    def ascii_to_index(self, ascii):
+        a = ord(ascii)
+        index = self.get_char(ascii) - 32
+        y = math.floor(index/15)
+        x = index%15
+        return (x,y)
+    
+    def get_char_image(self, char):
+        index = self.ascii_to_index(char)
+        return self.images[index[1]][index[0]]
+
+class Map:
+    def __init__(self) -> None:
+        self.map = [[chr(32+i+(x*15)) for i in range(15)] for x in range(8)] 
+
+    def draw(self, screen, sprites):
+        for y in range(8):
+            for x in range(15):
+                screen.blit(sprites.get_char_image(self.map[y][x]),(x*20,y*20))
+
 
 
 pygame.init()
+
+m = Map()
+pass
 
 # define the colours
 white = (255, 255, 255)
@@ -48,8 +87,8 @@ blue = (0, 0, 255)
 black = (0, 0, 0)
 
 # set the Dimensions
-width = 290 
-height = 405
+width = 300 
+height = 400
 
 # size of a block
 pixel = 64
@@ -57,11 +96,13 @@ pixel = 64
 # set Screen
 screen = pygame.display.set_mode((width, height))
 
-ss = spritesheet.spritesheet('fonts.png')
+ss = FontSpriteSheet()
 # Sprite is 16x16 pixels at location 0,0 in the file...
-images = ss.load_matrix([0, 20, 20, 20],(15,2), -1)
 
+images = ss.images
 player = Player(images)
+
+
 
 # set caption
 pygame.display.set_caption("CORONA SCARPER")
@@ -89,6 +130,10 @@ while running:
     # set the image on screen object
     screen.blit(backgroundImg, (0, 0))
 
+    screen.blit(ss.get_char_image('x'), (50, 50))
+    screen.blit(ss.get_char_image('h'), (70, 50))
+    screen.blit(ss.get_char_image(')'), (90, 50))
+    m.draw(screen, ss)
     # loop through all events
     for event in pygame.event.get():
             
